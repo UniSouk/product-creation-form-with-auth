530cf2d7482da92a71a9642c440441a331d7328f
Nirupama Singh <nirupama.singh@unisouk.com>
2025-12-09 19:52:40 +0530

    install dependency

--- next.config.ts
+++ next.config.ts
@@ -4,7 +4,7 @@ import "./lib/env";
 const nextConfig: NextConfig = {
   /* config options here */
     images: {
-    domains: ["unisouk-dev.s3.ap-south-1.amazonaws.com"], // add your S3 bucket hostname
+    domains: ["unisouk-dev.s3.ap-south-1.amazonaws.com"],
   },
 };
 
